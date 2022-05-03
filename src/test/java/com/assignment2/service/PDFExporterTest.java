ce6644846a1fea1e6ea43990668f967b099f2a93
Adrian <bindila.ioan@gmail.com>
2022-05-03 16:24:32 +0300

    Finish testing

--- src/test/java/com/assignment2/service/PDFExporterTest.java
+++ src/test/java/com/assignment2/service/PDFExporterTest.java
@@ -1,17 +1,26 @@
 package com.assignment2.service;
 
+import com.assignment2.dtos.FoodDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(MockitoJUnitRunner.class)
 class PDFExporterTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     void exportMenuPDF() {
+
     }
 }
